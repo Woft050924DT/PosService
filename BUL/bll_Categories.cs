9a73414054bdeb6cdd210b31bb7890fffe4e3b75
Nguyen Van Duc <duc362005lx2005xl@gmail.com>
2025-12-03 19:00:42 +0700

    Get-All-Categories

---
+++ BUL/bll_Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class bll_Categories
+    {
+        private DAL.dal_category dal = new DAL.dal_category();
+        public List<DTO.dto_Categories> GetAllCategories()
+        {
+            return dal.GetAll();
+        }
+
+    }
+}
---
+++ DAL/dal_category.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using DTO;
+
+namespace DAL
+{
+    public class dal_category
+    {
+        private string _conn = "Server=DESKTOP-TG67AE9;Database=HDV;Trusted_Connection=True;TrustServerCertificate=True;";
+        public List<dto_Categories> GetAll()
+        {
+            var list = new List<dto_Categories>();
+
+            SqlConnection sqlConnection = new(_conn);
+            using var conn = sqlConnection;
+            conn.Open();
+
+            var cmd = new SqlCommand("SELECT * FROM Categories", conn);
+
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var category = new dto_Categories
+                {
+                    CategoryId = reader["CategoryId"] != DBNull.Value ? Convert.ToInt32(reader["CategoryId"]) : 0,
+                    CategoryName = reader["CategoryName"]?.ToString(),
+                    Description = reader["Description"]?.ToString(),
+                    IsActive = reader["IsActive"] != DBNull.Value && Convert.ToBoolean(reader["IsActive"])
+                };
+
+                list.Add(category);
+            }
+
+            return list;
+        }
+
+
+    }
+}
