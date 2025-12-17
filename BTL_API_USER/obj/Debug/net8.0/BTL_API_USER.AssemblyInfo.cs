011d917f8042ff79b09f611f90b67e38c7cb5b50
HoangDEV <nhh19072005@gmail.com>
2025-12-17 17:02:40 +0700

    done base structure

--- BTL_API_USER/obj/Debug/net8.0/BTL_API_USER.AssemblyInfo.cs
+++ BTL_API_USER/obj/Debug/net8.0/BTL_API_USER.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTL_API_USER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd6795a2554020d70c88275cfe5c9461f302ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46f51825b24b2e28e67a948fbe527f3376e1422")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTL_API_USER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTL_API_USER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd6795a2554020d70c88275cfe5c9461f302ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46f51825b24b2e28e67a948fbe527f3376e1422")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
